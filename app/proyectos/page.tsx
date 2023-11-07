4806e43d7c6a2f953e4975958b2b11111c74f3e1
Carlos Cervantes <carlosgahelpulidocervantes@gmail.com>
2023-11-07 10:01:27 -0700

    Prueba para las tarjetas

--- app/proyectos/page.tsx
+++ app/proyectos/page.tsx
@@ -1,9 +1,15 @@
 import { title } from "@/components/primitives";
+import CardTest from "@/components/cardTest";
 
 export default function DocsPage() {
 	return (
 		<div>
 			<h1 className={title()}>Proyectos</h1>
+			<div className=" py-8 md:py-10 gap-2 grid grid-cols-3 mt-6 ">
+				<CardTest /><CardTest /><CardTest />
+				<CardTest /><CardTest /><CardTest />
+				<CardTest /><CardTest /><CardTest />
+			</div>
 		</div>
 	);
 }
--- components/cardTest.tsx
+++ components/cardTest.tsx
@@ -3,18 +3,18 @@ import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 
 export default function App() {
     return (
-        <Card className="py-2">
+        <Card className="py-2 min-w-full">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                 <p className="text-tiny uppercase font-bold">Prueba</p>
                 <small className="text-default-500">Uso de la card</small>
                 <h4 className="font-bold text-large">Imagen de ejemplo</h4>
             </CardHeader>
-            <CardBody className="overflow-visible py-2">
+            <CardBody className="overflow-hidden py-2">
                 <Image
                     alt="Card background"
                     className="object-cover rounded-xl"
-                    src="http://placekitten.com/g/300/300"
-                    width={300}
+                    src="http://placekitten.com/g/400/400"
+                    width={400}
                 />
             </CardBody>
         </Card>
