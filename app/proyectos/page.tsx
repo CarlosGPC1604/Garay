9ba7631cdcf582f416a06ac8662174f256b316b1
Carlos Cervantes <carlosgahelpulidocervantes@gmail.com>
2023-11-07 10:47:04 -0700

    Prueba de estilizado de layout

--- app/proyectos/page.tsx
+++ app/proyectos/page.tsx
@@ -3,9 +3,9 @@ import CardTest from "@/components/cardTest";
 
 export default function DocsPage() {
 	return (
-		<div>
+		<div className="py-8 md:py-10">
 			<h1 className={title()}>Proyectos</h1>
-			<div className=" py-8 md:py-10 gap-2 grid grid-cols-3 mt-6 ">
+			<div className="py-8 items-center justify-center gap-2 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
 				<CardTest /><CardTest /><CardTest />
 				<CardTest /><CardTest /><CardTest />
 				<CardTest /><CardTest /><CardTest />
