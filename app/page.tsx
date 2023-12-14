c17b4190b7950f41b353dcbef628f890567e50d0
AngelLuna17-code <luna17ahle@gmail.com>
2023-12-14 12:30:06 -0700

    imagenes y collaguee agregado

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import { title, subtitle } from "@/components/primitives";
 import LandingCards from "@/components/LandingCards";
 import Accordion from "@/components/Accordion";
 import AccordionImg from "@/components/AccordionImg";
+import Collague from "../components/collague"
 
 export default function Home() {
 	return (
 		<section>
+			{/* Hero */}
 			<div className="hero w-full py-[225px]">
 				<div className="container mx-auto text-center">
-					{/* TITULO */}
 					<h1 className={` ${title({ color: "red" })} text-[30px] sm:text-[36px] md:font-[40px] lg:text-[44px] xl:text-[48px] 2xl:[55px]`}>
 						Garay&nbsp;
 					</h1>
@@ -18,9 +19,9 @@ export default function Home() {
 					<h2 className={subtitle({})}>
 						Planeamos, proyectamos, construimos y supervisamos.
 					</h2>
-					{/* TITULO */}
 				</div>
 			</div>
+			{/* Hero */}
 			<div className="container mx-auto py-[40px] px-[10px]">
 				{/* ACERCA DE NOSOTROS */}
 				<div className="container mx-auto text-center mt-8">
@@ -28,11 +29,10 @@ export default function Home() {
 						Acerca de nosotros
 					</h2>
 					<div className="2xl:w-[65%] mx-auto">
-					<p className="text-wrap-balance mt-2">
-						En nuestro estudio, nos dedicamos apasionadamente a cada proyecto con el objetivo de trascender las expectativas y crear obras arquitectónicas que no solo sean funcionales y estéticamente agradables, sino que también cuenten una historia única. Nos esforzamos por fusionar la innovación con la sensibilidad hacia el entorno, incorporando prácticas sostenibles y soluciones creativas en cada diseño.
-					</p>
+						<p className="text-wrap-balance mt-2">
+							En nuestro estudio, nos dedicamos apasionadamente a cada proyecto con el objetivo de trascender las expectativas y crear obras arquitectónicas que no solo sean funcionales y estéticamente agradables, sino que también cuenten una historia única. Nos esforzamos por fusionar la innovación con la sensibilidad hacia el entorno, incorporando prácticas sostenibles y soluciones creativas en cada diseño.
+						</p>
 					</div>
-					
 				</div>
 				{/* ACERCA DE NOSOTROS */}
 				{/* LANDING CARDS */}
@@ -40,6 +40,9 @@ export default function Home() {
 					<LandingCards />
 				</div>
 				{/* LANDING CARDS */}
+				<div className="container mx auto">
+					<Collague />
+				</div>
 				{/* ACCORDION */}
 				<div className="container mx-auto mt-8 grid sm:grid-cols-1 md:grid-cols-2  gap-1">
 					<div>
---
+++ components/collague.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Collague() {
+    return (
+        <div class="grid sm:grid-cols-1 lg:grid-cols-3">
+            <div>
+                <img src="/collague/casa-ciruelos.jpg" alt="" />
+            </div>
+            <div>
+                <img src="/collague/Casa-Mari-Trini.jpg" alt="" />
+            </div>
+            <div>
+                <img src="/collague/casa-lindavista.jpg" alt="" />
+            </div>
+            <div>
+                <img src="/collague/Casa-Perla.jpg" alt="" />
+            </div>
+            <div>
+                <img src="/collague/Casa-Ojo-de-Agua.jpg" alt="" />
+            </div>
+            <div>
+                <img src="/collague/Casa-puerta-de-mangos.jpg" alt="" />
+            </div>
+            <div>
+                <img src="/collague/Casa-Senderos.jpg" alt="" />
+            </div>
+            <div>
+                <img src="/collague/Fotos.jpeg" alt="" />
+            </div>
+            <div>
+                <img src="/collague/Planta-Tomates.jpg" alt="" />
+            </div>
+        </div>
+    );
+}
+
+export default Collague;
