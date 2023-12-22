f29c972746b3854aa0a91bea26b80d41591c2f1a
AngelLuna17-code <luna17ahle@gmail.com>
2023-12-22 12:52:58 -0700

    blog edited

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,8 +4,8 @@ export default function BlogLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<section className="items-center justify-center gap-4">
-			<div className="inline-block max-w-lg text-center justify-center ">
+		<section>
+			<div>
 				{children}
 			</div>
 		</section>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,8 +2,10 @@ import { title } from "@/components/primitives";
 
 export default function BlogPage() {
 	return (
-		<div>
-			<h1 className={title()}>Blog</h1>
+		<div className="h-full text-center py-[1000px]">
+			<h1 className={` ${title({ color: "red" })} text-[30px] sm:text-[36px] md:font-[40px] lg:text-[44px] xl:text-[48px] 2xl:[55px]`}>
+				Blog
+			</h1>
 		</div>
 	);
 }
