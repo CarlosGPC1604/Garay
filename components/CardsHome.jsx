6a26bcdfba35f5c658fa5897e9076295ea4547b4
AngelLuna17-code <luna17ahle@gmail.com>
2024-01-23 14:12:30 -0700

    estilado cards

--- components/CardsHome.jsx
+++ components/CardsHome.jsx
@@ -23,7 +23,7 @@ function CardsHome() {
             {/* Cards */}
             <div className="grid grid-cols-2 gap-5 lg:gap-2 overflow-hidden">
                 {/* Card-1 */}
-                <div className="mx-auto flex items-center justify-center overflow-hidden bg-[#1b1b1b] h-[150px] w-[150px] sm:h-[250px] sm:w-[250px] lg:h-auto lg:w-[100%]">
+                <div className="mx-auto flex items-center justify-center overflow-hidden bg-[#1b1b1b] hover:bg-[#2c2a2a] h-[150px] w-[150px] sm:h-[250px] sm:w-[250px] lg:h-auto lg:w-[100%]">
                     <div >
                         <svg className='text-[#de2729] transition-transform duration-300 ease-in-out transform hover:translate-y-3' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M15 .5a.5.5 0 0 0-.724-.447l-8 4A.5.5 0 0 0 6 4.5v3.14L.342 9.526A.5.5 0 0 0 0 10v5.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V14h1v1.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5zM2 11h1v1H2zm2 0h1v1H4zm-1 2v1H2v-1zm1 0h1v1H4zm9-10v1h-1V3zM8 5h1v1H8zm1 2v1H8V7zM8 9h1v1H8zm2 0h1v1h-1zm-1 2v1H8v-1zm1 0h1v1h-1zm3-2v1h-1V9zm-1 2h1v1h-1zm-2-4h1v1h-1zm3 0v1h-1V7zm-2-2v1h-1V5zm1 0h1v1h-1z" />
@@ -37,7 +37,7 @@ function CardsHome() {
                 </div>
                 {/* Card-1 */}
                 {/* Card-2 */}
-                <div className="mx-auto flex items-center justify-center overflow-hidden bg-[#1b1b1b] h-[150px] w-[150px] sm:h-[250px] sm:w-[250px] lg:h-auto lg:w-[100%]">
+                <div className="mx-auto flex items-center justify-center overflow-hidden bg-[#1b1b1b] hover:bg-[#2c2a2a] h-[150px] w-[150px] sm:h-[250px] sm:w-[250px] lg:h-auto lg:w-[100%]">
                     <div >
                         <svg className='text-[#de2729] transition-transform duration-300 ease-in-out transform hover:translate-y-3' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
@@ -51,7 +51,7 @@ function CardsHome() {
                 </div>
                 {/* Card-2 */}
                 {/* Card-3 */}
-                <div className="mx-auto flex items-center justify-center overflow-hidden bg-[#1b1b1b] h-[150px] w-[150px] sm:h-[250px] sm:w-[250px] lg:h-auto lg:w-[100%]">
+                <div className="mx-auto flex items-center justify-center overflow-hidden bg-[#1b1b1b] hover:bg-[#2c2a2a] h-[150px] w-[150px] sm:h-[250px] sm:w-[250px] lg:h-auto lg:w-[100%]">
                     <div >
                         <svg className='text-[#de2729] transition-transform duration-300 ease-in-out transform hover:translate-y-3' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M1 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h5v-1H2v-1h4v-1H4v-1h2v-1H2v-1h4V9H4V8h2V7H2V6h4V2h1v4h1V4h1v2h1V2h1v4h1V4h1v2h1V2h1v4h1V1a1 1 0 0 0-1-1z" />
@@ -65,7 +65,7 @@ function CardsHome() {
                 </div>
                 {/* Card-3 */}
                 {/* Card-4 */}
-                <div className="mx-auto flex items-center justify-center overflow-hidden bg-[#1b1b1b] h-[150px] w-[150px] sm:h-[250px] sm:w-[250px] lg:h-auto lg:w-[100%]">
+                <div className="mx-auto flex items-center justify-center overflow-hidden bg-[#1b1b1b] hover:bg-[#2c2a2a] h-[150px] w-[150px] sm:h-[250px] sm:w-[250px] lg:h-auto lg:w-[100%]">
                     <div >
                         <svg className='text-[#de2729] transition-transform duration-300 ease-in-out transform hover:translate-y-3' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M10.157 5.968h-.844v4.06h.844c1.116 0 1.621-.667 1.621-2.02 0-1.354-.51-2.04-1.621-2.04" />
