281b2c7b7c7ad2b6ffbc0781a1db3c72b2079dd0
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-04 13:00:52 -0700

    agregado mapa

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "../../components/Hero";
 import Cards from "../../components/Cards"
+import Mapa from "../../components/Mapa"
 
 export default function Home() {
   return (
     <section>
       <Hero />
       <Cards />
+      <Mapa />
     </section>
   );
 }
