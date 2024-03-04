01d82b37077b70dcfe32dc13bfd05c8f63b99188
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-04 14:40:47 -0700

    agregando texto en carrudel

--- components/RenderCarrucel.jsx
+++ components/RenderCarrucel.jsx
@@ -6,29 +6,17 @@ function RenderCarousel() {
     const [fadeAnimation, setFadeAnimation] = useState(false);
     const [loadedImages, setLoadedImages] = useState([]);
 
-    const content = [
-        {
-            text: "Soluciones arquitectónicas profesionales",
-            image: "/Renders/IMG-20240105-WA0044.webp",
-        },
-        {
-            text: "Proyectos originales y creativos",
-            image: "/Renders/IMG-20240105-WA0030.webp",
-        },
-        {
-            text: "Innovando en cada proyecto",
-            image: "/Renders/IMG-20240105-WA0108.webp",
-        },
-        {
-            text: "Mejorando constantemente",
-            image: "/Renders/IMG-20240105-WA0024.webp",
-        },
+    const images = [
+        "/Renders/IMG-20240105-WA0044.webp",
+        "/Renders/IMG-20240105-WA0030.webp",
+        "/Renders/IMG-20240105-WA0108.webp",
+        "/Renders/IMG-20240105-WA0024.webp",
     ];
 
     const handleNextImage = () => {
         setFadeAnimation(true);
         setTimeout(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % content.length);
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
             setFadeAnimation(false);
         });
     };
@@ -36,7 +24,7 @@ function RenderCarousel() {
     const handlePrevImage = () => {
         setFadeAnimation(true);
         setTimeout(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex - 1 + content.length) % content.length);
+            setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
             setFadeAnimation(false);
         });
     };
@@ -53,35 +41,46 @@ function RenderCarousel() {
         };
     }, [currentImageIndex]);
 
-    const currentContent = content[currentImageIndex];
-
     return (
         <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-5'>
-            <div>
-
+            <div className='flex items-center justify-center text-white'>
+                <div className='text-center lg:text-right text-balance'>
+                    <h2 className="text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
+                        Perspectivas&nbsp;
+                    </h2>
+                    <h2 className="text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
+                        Tridimensionales
+                    </h2>
+                    <p className='text-balance mt-2'>
+                        Ofrecemos servicios de renders fotorealistas, visualización 3D, recorridos virtuales y presentaciones impactantes. Nuestro equipo comprometido y experimentado da vida a tus ideas arquitectónicas con creatividad y precisión técnica. Destaca entre la competencia con imágenes cautivadoras. Colabora con nosotros para proyectos personalizados y entregas puntuales. Transforma tus conceptos en experiencias visuales impresionantes. ¡Contáctanos hoy para obtener más información!
+                    </p>
+                    <a href="/renders">
+                        <button className="mt-4 cursor-pointer text-white px-5 py-4 lg:right-10 bg-[#e12527] hover:bg-[#e0323c]">
+                            Descubre más
+                        </button>
+                    </a>
+                </div>
             </div>
             {/* Carrusel */}
-            <div>
+            <div className="relative">
                 <button
                     onClick={handlePrevImage}
-                    className="w-[50px] text-white hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 rounded-full">
+                    className="absolute left-0 top-1/2 transform -translate-y-1/2 w-[50px] h-[50px] text-white hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 z-10">
                     &lt;
                 </button>
-                <div>
-                    {content.map((item, index) => (
-                        <img
-                            key={index}
-                            src={item.image}
-                            alt={item.text}
-                            style={{ display: index === currentImageIndex ? 'block' : 'none' }}
-                        />
-                    ))}
-                </div>
                 <button
                     onClick={handleNextImage}
-                    className="w-[50px] text-white hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 rounded-full">
+                    className="absolute right-0 top-1/2 transform -translate-y-1/2 w-[50px] h-[50px] text-white hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 z-10">
                     &gt;
                 </button>
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt={`Image ${index + 1}`}
+                        style={{ display: index === currentImageIndex ? 'block' : 'none' }}
+                    />
+                ))}
             </div>
             {/* Carrusel */}
         </div>
