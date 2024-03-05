bd9ca7703e949a34175ef238d5b8c8a64096243e
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-05 12:51:24 -0700

    estilando componente

--- components/ProyectCards.jsx
+++ components/ProyectCards.jsx
@@ -1,13 +1,14 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 
-// Carousel component
+// Carousel
 function RenderCarousel({ images }) {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [fadeAnimation, setFadeAnimation] = useState(false);
     const [loadedImages, setLoadedImages] = useState([]);
+    // Carousel
 
-    // Function to handle next image
+    // Flechas
     const handleNextImage = () => {
         setFadeAnimation(true);
         setTimeout(() => {
@@ -16,7 +17,6 @@ function RenderCarousel({ images }) {
         });
     };
 
-    // Function to handle previous image
     const handlePrevImage = () => {
         setFadeAnimation(true);
         setTimeout(() => {
@@ -24,8 +24,9 @@ function RenderCarousel({ images }) {
             setFadeAnimation(false);
         });
     };
+    // Flechas
 
-    // Rendering the carousel component
+    {/* Carrusel */ }
     return (
         <div className="relative">
             {/* Previous button */}
@@ -40,7 +41,7 @@ function RenderCarousel({ images }) {
                 className="absolute right-0 top-1/2 transform -translate-y-1/2 w-[50px] h-[50px] text-white hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 z-10">
                 &gt;
             </button>
-            {/* Render images */}
+            {/* Imagenes */}
             {images.map((image, index) => (
                 <img
                     key={index}
@@ -49,15 +50,16 @@ function RenderCarousel({ images }) {
                     style={{ display: index === currentImageIndex ? 'block' : 'none' }}
                 />
             ))}
+            {/* Imagenes */}
         </div>
     );
+    {/* Carrusel */ }
 }
 
 // Main component
 const ProyectCards = () => {
     const [openModalIndex, setOpenModalIndex] = useState(null);
 
-    // Effect for handling escape key to close modal
     useEffect(() => {
         const handleEsc = (event) => {
             if (event.keyCode === 27) {
@@ -72,42 +74,41 @@ const ProyectCards = () => {
         };
     }, []);
 
-    // Function to open modal
+
     const handleOpenModal = (index) => {
         setOpenModalIndex(index);
     };
 
-    // Function to close modal
+
     const handleCloseModal = () => {
         setOpenModalIndex(null);
     };
 
-    // Data for projects
+
     const projects = [
         {
-            images: [ // Example images for Project 1
+            images: [
                 "/carruseles/proyecto-1/item-1.webp",
-                "/Project1/Image2.webp",
-                "/Project1/Image3.webp",
+                "/carruseles/proyecto-1/item-2.webp",
+                "/carruseles/proyecto-1/item-3.webp",
             ],
         },
         {
-            images: [ // Example images for Project 2
-                "/Project2/Image1.webp",
-                "/Project2/Image2.webp",
-                "/Project2/Image3.webp",
+            images: [
+                "/carruseles/proyecto-2/item-1.webp",
+                "/carruseles/proyecto-2/item-2.webp",
+                "/carruseles/proyecto-2/item-3.webp",
             ],
         },
         {
-            images: [ // Example images for Project 3
-                "/Project3/Image1.webp",
-                "/Project3/Image2.webp",
-                "/Project3/Image3.webp",
+            images: [
+                "/carruseles/proyecto-3/item-1.webp",
+                "/carruseles/proyecto-3/item-2.webp",
+                "/carruseles/proyecto-3/item-3.webp",
             ],
         },
     ];
 
-    // Rendering the main component
     return (
         <section>
             <div className='container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5 py-[50px] px-[15px]'>
@@ -122,18 +123,14 @@ const ProyectCards = () => {
                 {/* Render project cards */}
             </div>
 
-            {/* Render modals */}
+            {/* Modal */}
             {projects.map((project, index) => (
                 <div key={index}>
                     {openModalIndex === index && (
                         <div className="modal-overlay z-10 fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
                             <div className="modal bg-black rounded-[15px] w-[360px] sm:w-[600px] md:w-[720px] lg:w-[1000px] xl:w-[1200px] 2xl:w-[1300px]">
                                 <div className='p-5' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
-                                    <div>
-                                        <p className='title text-2xl'>
-                                            {project.title}
-                                        </p>
-                                    </div>
+                                    {/* cerrar modal */}
                                     <button
                                         className="text-white bg-transparent border-none cursor-pointer"
                                         onClick={handleCloseModal}
@@ -142,26 +139,21 @@ const ProyectCards = () => {
                                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
                                         </svg>
                                     </button>
+                                    {/* cerrar modal */}
                                 </div>
-                                <div>
-                                    <RenderCarousel images={project.images} />
-
+                                {/* Contenido del modal */}
+                                <div className="w-[80%] mx-auto">
+                                    <RenderCarousel
+                                     images={project.images} 
+                                     />
                                 </div>
-                                <div className='p-5'>
-                                    <div className='flex justify-end'>
-                                        <button
-                                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                                            onClick={handleCloseModal}>
-                                            Cerrar
-                                        </button>
-                                    </div>
-                                </div>
+                                {/* Contenido del modal */}
                             </div>
                         </div>
                     )}
                 </div>
             ))}
-            {/* Render modals */}
+            {/* Modal */}
         </section>
     );
 }
