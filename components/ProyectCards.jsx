31c49f3987e16f4580dae06f60e35f9efc9fb5a4
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-05 14:41:02 -0700

    trabajando cards

--- components/ProyectCards.jsx
+++ components/ProyectCards.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useState, useEffect } from 'react';
 
 // Carousel
@@ -6,9 +6,7 @@ function RenderCarousel({ images }) {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [fadeAnimation, setFadeAnimation] = useState(false);
     const [loadedImages, setLoadedImages] = useState([]);
-    // Carousel
 
-    // Flechas
     const handleNextImage = () => {
         setFadeAnimation(true);
         setTimeout(() => {
@@ -24,9 +22,7 @@ function RenderCarousel({ images }) {
             setFadeAnimation(false);
         });
     };
-    // Flechas
 
-    {/* Carrusel */ }
     return (
         <div className="relative">
             {/* Previous button */}
@@ -41,7 +37,7 @@ function RenderCarousel({ images }) {
                 className="absolute right-0 top-1/2 transform -translate-y-1/2 w-[50px] h-[50px] text-white hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 z-10">
                 &gt;
             </button>
-            {/* Imagenes */}
+            {/* Images */}
             {images.map((image, index) => (
                 <img
                     className='w-auto h-[300px] lg:h-[650px] object-contain mx-auto'
@@ -51,10 +47,9 @@ function RenderCarousel({ images }) {
                     style={{ display: index === currentImageIndex ? 'block' : 'none' }}
                 />
             ))}
-            {/* Imagenes */}
+            {/* Images */}
         </div>
     );
-    {/* Carrusel */ }
 }
 
 // Main component
@@ -93,6 +88,7 @@ const ProyectCards = () => {
                 "/carruseles/proyecto-1/item-2.webp",
                 "/carruseles/proyecto-1/item-3.webp",
             ],
+            title: "Titulo de proyecto 1"
         },
         {
             images: [
@@ -100,6 +96,7 @@ const ProyectCards = () => {
                 "/carruseles/proyecto-2/item-2.webp",
                 "/carruseles/proyecto-2/item-3.webp",
             ],
+            title: "Titulo de proyecto 2"
         },
         {
             images: [
@@ -107,50 +104,57 @@ const ProyectCards = () => {
                 "/carruseles/proyecto-3/item-2.webp",
                 "/carruseles/proyecto-3/item-3.webp",
             ],
+            title: "Titulo de proyecto 3"
         },
     ];
 
     return (
         <section>
             <div className='container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5 py-[50px] px-[15px]'>
-                {/* Render project cards */}
+                {/* Open modal */}
                 {projects.map((project, index) => (
-                    <div className="rounded-[15px]" key={index}>
-                        <button className='cursor-pointer' onClick={() => handleOpenModal(index)}>
-                            Abrir modal
-                        </button>
+                    <div key={index} className='relative group cursor-pointer' onClick={() => handleOpenModal(index)}>
+                        <img
+                          className="rounded-[15px]"
+                          src={project.images[0]}
+                          alt={`Open`}
+                          loading="lazy"
+                        />
+                        <div className="absolute inset-x-0 bottom-0 py-[15px] rounded-b-[15px] flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-10" style={{ background: 'rgba(0, 0, 0, 0.4)', backdropFilter: 'blur(10px)' }}>
+                          <p className="text-white text-center">{project.title}</p>
+                        </div>
                     </div>
                 ))}
-                {/* Render project cards */}
+                {/* Open modal */}
             </div>
-{/* Modal */}
-{projects.map((project, index) => (
-    <div key={index}>
-        {openModalIndex === index && (
-            <div className="modal-overlay z-10 fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-                <div className="w-[345px] sm:w-[600px] md:w-[720px] lg:w-[1000px] xl:w-[1200px] 2xl:w-[1300px] flex flex-col" style={{ background: 'rgba(0, 0, 0, 0.4)', backdropFilter: 'blur(10px)' }}>
-                    {/* Contenido del modal */}
-                    <div className="flex justify-end">
-                        <button
-                            className="text-white bg-transparent border-none cursor-pointer m-4"
-                            onClick={handleCloseModal}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="w-full mx-auto pb-[60px]  lg:pb-[80px]">
-                        <RenderCarousel images={project.images} />
-                    </div>
-                    {/* Contenido del modal */}
+            {/* Modal */}
+            {projects.map((project, index) => (
+                <div key={index}>
+                    {openModalIndex === index && (
+                        <div className="modal-overlay z-10 fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+                            <div className="w-[345px] sm:w-[600px] md:w-[720px] lg:w-[1000px] xl:w-[1200px] 2xl:w-[1300px] flex flex-col" style={{ background: 'rgba(0, 0, 0, 0.4)', backdropFilter: 'blur(10px)' }}>
+                                {/* Modal content */}
+                                <div className="flex justify-end">
+                                    <button
+                                        className="text-white bg-transparent border-none cursor-pointer m-4"
+                                        onClick={handleCloseModal}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
+                                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div className="w-full mx-auto pb-[60px]  lg:pb-[80px]">
+                                    <RenderCarousel
+                                        images={project.images}
+                                    />
+                                </div>
+                                {/* Modal content */}
+                            </div>
+                        </div>
+                    )}
                 </div>
-            </div>
-        )}
-    </div>
-))}
-{/* Modal */}
-
-
+            ))}
+            {/* Modal */}
         </section>
     );
 }
