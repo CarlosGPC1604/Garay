b6e3e131e05e2f4a0e6e8beb55a9e81c072de30f
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-06 10:26:06 -0700

    empezando con collague

---
+++ components/Collague.jsx
@@ -0,0 +1,55 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+
+const Collague = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsOpen(false);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.keyCode === 27) { // 27 es el código de la tecla Esc
+            handleCloseModal();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    return (
+        <div>
+            <button onClick={handleOpenModal}>Abrir Modal</button>
+            {isOpen && (
+                 <div className="modal-overlay z-10 fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+                 <div className="w-[345px] sm:w-[600px] md:w-[720px] lg:w-[1000px] xl:w-[1200px] 2xl:w-[1300px] flex flex-col" style={{ background: 'rgba(0, 0, 0, 0.4)', backdropFilter: 'blur(10px)' }}>
+                     {/* Modal content */}
+                     <div className="flex justify-end">
+                         <button
+                             className="text-white bg-transparent border-none cursor-pointer m-4"
+                             onClick={handleCloseModal}>
+                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
+                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                             </svg>
+                         </button>
+                     </div>
+                     <div className="w-full mx-auto pb-[60px] lg:pb-[80px]">
+                        {/* Contenido del modal */}
+                     </div>
+                     {/* Modal content */}
+                 </div>
+             </div>
+            )}
+        </div>
+    );
+};
+
+export default Collague;
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Hero from "../../components/Hero";
 import Cards from "../../components/Cards"
 import Mapa from "../../components/Mapa"
 import RenderCarrucel from "../../components/RenderCarrucel";
+import Collague from "../../components/Collague";
 
 export default function Home() {
   return (
     <section>
       <Hero />
       <Cards />
+      <Collague />
       <RenderCarrucel />
       <Mapa />
     </section>
