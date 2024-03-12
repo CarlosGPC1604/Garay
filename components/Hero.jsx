7b7066aac21e40f81bc1827c9af4bd0a81d30528
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-12 14:54:28 -0700

    cambio en el hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -86,11 +86,11 @@ function Hero() {
                 />
                 <h1 className="text-[33px] sm:text-[36px] md:font-[40px] lg:text-[44px] xl:text-[48px] 2xl:[55px] font-medium text-[#bd1622]">
                     Garay&nbsp;
-                    <span className='text-black'>
+                    <span className='text-white'>
                         Arquitectos
                     </span>
                 </h1>
-                <p className='text-2xl text-black'>
+                <p className='text-2xl text-white'>
                     {currentContent.text}
                 </p>
             </div>
