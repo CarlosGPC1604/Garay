16a64c8488d2ee479f0dc247ff462722cb48f371
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-14 14:28:07 -0700

    estilado y corregido espaciados

--- components/RenderCarrucel.jsx
+++ components/RenderCarrucel.jsx
@@ -49,54 +49,56 @@ function RenderCarousel() {
     }, [currentImageIndex]);
 
     return (
-        <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2  py-[50px] px-[15px]'>
-            <div className=' glass p-4 flex items-center justify-center text-black'>
-                <div className='text-center lg:text-right text-balance'>
-                    <h2 className="text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
-                        Perspectivas&nbsp;
-                    </h2>
-                    <h2 className="text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
-                        Tridimensionales
-                    </h2>
-                    <p className='text-balance mt-3'>
-                        Ofrecemos servicios de renders fotorealistas, visualización 3D, recorridos virtuales y presentaciones impactantes. Nuestro equipo comprometido y experimentado da vida a tus ideas arquitectónicas con creatividad y precisión técnica. Destaca entre la competencia con imágenes cautivadoras. Colabora con nosotros para proyectos personalizados y entregas puntuales. Transforma tus conceptos en experiencias visuales impresionantes. ¡Contáctanos hoy para obtener más información!
-                    </p>
-                    <a href="/renders">
-                        <button className="mt-4 cursor-pointer text-black px-5 py-4 lg:right-10">
-                            Descubre más
+        <section className='py-[50px] px-[15px] bg-[#c5c5c5] dark:bg-[#231f20]'>
+            <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-5'>
+                <div className=' glass p-4 flex items-center justify-center'>
+                    <div className='text-center lg:text-right text-balance'>
+                        <h2 className="text-[#231f20] dark:text-white text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
+                            Perspectivas&nbsp;
+                        </h2>
+                        <h2 className="text-[#c21e25] text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
+                            Tridimensionales
+                        </h2>
+                        <p className='text-[#231f20] dark:text-white text-balance mt-3'>
+                            Ofrecemos servicios de renders fotorealistas, visualización 3D, recorridos virtuales y presentaciones impactantes. Nuestro equipo comprometido y experimentado da vida a tus ideas arquitectónicas con creatividad y precisión técnica. Destaca entre la competencia con imágenes cautivadoras. Colabora con nosotros para proyectos personalizados y entregas puntuales. Transforma tus conceptos en experiencias visuales impresionantes. ¡Contáctanos hoy para obtener más información!
+                        </p>
+                        <a href="/renders">
+                            <button className="text-[#231f20] dark:text-white mt-8 hover:underline hover:text-[#c21e25] dark:hover:text-[#c21e25]">
+                                Descubre más
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                {/* Carrusel */}
+                <div className="flex items-center justify-center">
+                    <div className="relative">
+                        <button
+                            onClick={handlePrevImage}
+                            className="absolute left-0 top-1/2 transform -translate-y-1/2 w-[50px] h-[50px] text-black hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 z-10">
+                            &lt;
                         </button>
-                    </a>
+                        <button
+                            onClick={handleNextImage}
+                            className="absolute right-0 top-1/2 transform -translate-y-1/2 w-[50px] h-[50px] text-black hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 z-10">
+                            &gt;
+                        </button>
+                        {images.map((image, index) => (
+                            <Image
+                                className='w-full h-auto object-contain mx-auto'
+                                height={0}
+                                width={0}
+                                key={index}
+                                src={image}
+                                alt={`Image ${index + 1}`}
+                                style={{ display: index === currentImageIndex ? 'block' : 'none' }}
+                                sizes="100vw"
+                            />
+                        ))}
+                    </div>
                 </div>
+                {/* Carrusel */}
             </div>
-            {/* Carrusel */}
-            <div className="flex items-center justify-center">
-                <div className="relative">
-                    <button
-                        onClick={handlePrevImage}
-                        className="absolute left-0 top-1/2 transform -translate-y-1/2 w-[50px] h-[50px] text-black hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 z-10">
-                        &lt;
-                    </button>
-                    <button
-                        onClick={handleNextImage}
-                        className="absolute right-0 top-1/2 transform -translate-y-1/2 w-[50px] h-[50px] text-black hover:text-black bg-transparent hover:bg-gray-50 bg-opacity-70 hover:bg-opacity-80 transition-all duration-300 z-10">
-                        &gt;
-                    </button>
-                    {images.map((image, index) => (
-                        <Image
-                            className='w-auto h-[400px] object-contain mx-auto'
-                            height={0}
-                            width={0}
-                            key={index}
-                            src={image}
-                            alt={`Image ${index + 1}`}
-                            style={{ display: index === currentImageIndex ? 'block' : 'none' }}
-                            sizes="100vw"
-                        />
-                    ))}
-                </div>
-            </div>
-            {/* Carrusel */}
-        </div>
+        </section>
     );
 }
 
