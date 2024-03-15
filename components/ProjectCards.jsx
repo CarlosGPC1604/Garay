794443cca56a75fcbda72ca33e5cca0ecd090381
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-15 12:22:43 -0700

    estilando pagina de proyectos

--- components/ProjectCards.jsx
+++ components/ProjectCards.jsx
@@ -230,7 +230,7 @@ const ProjectCards = () => {
     ];
 
     return (
-        <section>
+        <section className='bg-[#c5c5c5] dark:bg-[#231f20]'>
             <div className='container mx-auto grid grid-cols-1 lg:grid-cols-5 gap-5 py-[50px] px-[15px]'>
                 {/* Open modal */}
                 {projects.map((project, index) => (
@@ -260,7 +260,7 @@ const ProjectCards = () => {
                                 {/* Modal content */}
                                 <div className="flex justify-end">
                                     <button
-                                        className="text-black bg-transparent border-none cursor-pointer m-4"
+                                        className="text-white hover:text-black bg-transparent border-none cursor-pointer m-4"
                                         onClick={handleCloseModal}>
                                         <svg xmlns="https://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
                                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
