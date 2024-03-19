8630fc221be194b3598b34bf171ee91a218b5985
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-19 14:22:19 -0700

    estilado

--- src/app/contacto/page.js
+++ src/app/contacto/page.js
@@ -15,10 +15,11 @@ export default function Contacto() {
     return (
         <section className="bg-[#c5c5c5] dark:bg-[#231f20]" style={divStyle}>
             {/* Hero */}
-            <div className='w-full py-[150px] flex items-center justify-center contacto hero-bg'>
+            <div className='w-full py-[150px] flex items-center justify-center hero-bg proyectos'>
                 <div className='text-center'>
-                    <h1 className="text-[#c21e25] text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
+                    <h1 className="text-[33px] sm:text-[36px] md:font-[40px] lg:text-[44px] xl:text-[48px] 2xl:[55px] font-medium text-[#bd1622]">
                         Contactanos
+
                     </h1>
                     <p className="text-lg text-white">
                         Contactanos y te ayudamos a hacerlo realidad
--- src/app/proyectos/page.js
+++ src/app/proyectos/page.js
@@ -5,12 +5,12 @@ export default function Proyectos() {
   return (
     <section>
       {/* Hero */}
-      <div className='w-full py-[150px] flex items-center justify-center'>
+      <div className='w-full py-[150px] flex items-center justify-center hero-bg proyectos'>
         <div className='text-center'>
           <h1 className="text-[33px] sm:text-[36px] md:font-[40px] lg:text-[44px] xl:text-[48px] 2xl:[55px] font-medium text-[#bd1622]">
             Descubre nuestro
             <span className='text-white'>
-              portafolio
+              &nbsp;portafolio
             </span>
           </h1>
           <p className='text-2xl text-white'>
