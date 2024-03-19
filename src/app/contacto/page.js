6ff2b8ecb3491aad0103cb2e7bf8d0484daf243d
AngelLuna17-code <luna17ahle@gmail.com>
2024-03-19 13:58:09 -0700

    estilado contacto

--- src/app/contacto/page.js
+++ src/app/contacto/page.js
@@ -1,22 +1,31 @@
-
 import ContactCards from "../../../components/ContactCards";
 import Mapa from "../../../components/Mapa";
 
 export default function Contacto() {
+
+    const divStyle = {
+        backgroundImage: 'url(./dark-bg.svg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: '50% 50%',
+        overflow: 'hidden',
+        transition: 'background-image 0.5s ease-in-out',
+    };
+
     return (
-        <section>
-            {/* Titulo */}
-            <div className="container mx-auto py-[125px]">
-                <div className="text-center">
-                    <h1 className="text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
+        <section className="bg-[#c5c5c5] dark:bg-[#231f20]" style={divStyle}>
+            {/* Hero */}
+            <div className='w-full py-[150px] flex items-center justify-center contacto hero-bg'>
+                <div className='text-center'>
+                    <h1 className="text-[#c21e25] text-[22px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[40px]">
                         Contactanos
                     </h1>
-                    <p className="text-lg">
+                    <p className="text-lg text-white">
                         Contactanos y te ayudamos a hacerlo realidad
                     </p>
                 </div>
             </div>
-            {/* Titulo */}
+            {/* Hero */}
             {/* Cards */}
             <ContactCards />
             {/* Cards */}
--- src/app/proyectos/page.js
+++ src/app/proyectos/page.js
@@ -1,25 +1,11 @@
 import ProjectCards from "../../../components/ProjectCards";
 
-
 export default function Proyectos() {
 
-  const divStyle = {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(./proyects-paralax.webp)`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    backgroundPosition: '50% 50%',
-    overflow: 'hidden',
-    backdropFilter: 'blur(0)',
-    WebkitBackdropFilter: 'blur(0)',
-    backgroundAttachment: "fixed",
-    transition: 'background-image 0.5s ease-in-out',
-  };
-  
   return (
     <section>
       {/* Hero */}
-      <div className='w-full py-[150px] flex items-center justify-center' style={divStyle}>
+      <div className='w-full py-[150px] flex items-center justify-center'>
         <div className='text-center'>
           <h1 className="text-[33px] sm:text-[36px] md:font-[40px] lg:text-[44px] xl:text-[48px] 2xl:[55px] font-medium text-[#bd1622]">
             Descubre nuestro
